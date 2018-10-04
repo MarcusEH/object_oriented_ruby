4920235c86ee6f0cdffc752cb5ef6c23295dc6be
Marcus Hamill <marcusehamill@gmail.com>
2018-10-03 21:01:46 -0500

    added item hashes to store

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+hat = {:name => "baseball hat", :color => "red", :price => 20}
+shirt = {name: 'tee shirt', color: 'blue', price: '30'}
+pants = {name: 'jeans', color: 'blue', price: '50'}
+
+p hat[:name]
